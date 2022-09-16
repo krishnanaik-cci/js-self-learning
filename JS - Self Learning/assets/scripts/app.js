e7c6dc38425a5de37480e3e741ac29099752f4a9
CREATIVECAPSULE\krishna.naik <krishna.naik@creativecapsule.com>
2022-09-16 10:05:21 +0530

    section6:feat:Add 'Rock, Paper, Scissor' game

--- JS - Self Learning/assets/scripts/app.js
+++ JS - Self Learning/assets/scripts/app.js
@@ -9,8 +9,15 @@ const MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
 const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
 const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_BONUS_LIFE_APPLIED = "BONUS_LIFE_APPLIED";
 const LOG_HEAL_PLAYER = "HEAL_PLAYER";
 const LOG_GAME_OVER = "GAME_OVER";
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSOR = "SCISSOR";
+const DRAW = "DRAW!";
+const PLAYER_WON = "PLAYER WON!";
+const COMPUTER_WON = "COMPUTER WON!";
 let isBonusHealthAvailable = true;
 let logData = [];
 let chosenMaxLife;
@@ -27,7 +34,7 @@ function getMaxLifeValue() {
 }
 
 try {
-  chosenMaxLife = getMaxLifeValue();
+  chosenMaxLife = getMaxLifeValue(); //this will throw an exception
 } catch (error) {
   // executes becuase getMaxLifeValue funtion has an error
   console.log("Error = " + error.message);
@@ -41,7 +48,7 @@ let currentPlayerHealth = chosenMaxLife;
 // sets the UI of Monster and Player health bar
 adjustHealthBars(chosenMaxLife);
 
-function reset() {
+function restartGameHandler() {
   currentMonsterHealth = chosenMaxLife;
   currentPlayerHealth = chosenMaxLife;
   resetGame(chosenMaxLife);
@@ -64,7 +71,6 @@ function writeToLog(ev, val, playerHealth, monsterHealth) {
       break;
     case LOG_EVENT_PLAYER_STRONG_ATTACK:
       logDetails.target = "MONSTER";
-      break;
   }
   logData.push(logDetails);
 }
@@ -72,9 +78,8 @@ function writeToLog(ev, val, playerHealth, monsterHealth) {
 function endRound() {
   let initialPlayerHealth = currentPlayerHealth;
   // monster will also attack while increasing the health
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE); // this updates the progressbar ui
   currentPlayerHealth -= playerDamage;
-  console.log('currentPlayerHealth ',currentPlayerHealth);
   // how do we know if the attack is normal or strong
   writeToLog(
     LOG_EVENT_MONSTER_ATTACK,
@@ -90,11 +95,17 @@ function endRound() {
     // sets previous currentPlayerHealth value before it became negative/zero
     currentPlayerHealth = initialPlayerHealth;
     setPlayerHealth(initialPlayerHealth);
-    alert("Bonus life applied!"); //show alert only when 
+    alert("Bonus life applied!"); //show alert only when
+    writeToLog(
+      LOG_BONUS_LIFE_APPLIED,
+      initialPlayerHealth,
+      currentPlayerHealth,
+      currentMonsterHealth
+    );
   }
   // if currentMonsterHealth is <=0 you win but if currentPlayerHelth also goes down to zero then
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("You Won!");
+    declareWinner("PLAYER WON!");
     writeToLog(
       LOG_GAME_OVER,
       "PLAYER WON!",
@@ -103,7 +114,7 @@ function endRound() {
     );
     // if currentPlayerHealth is <=0 you lost but if currentMonsterHealth also goes down to zero then
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("You Lost!");
+    declareWinner("MONSTER WON!");
     writeToLog(
       LOG_GAME_OVER,
       "MONSTER WON!",
@@ -111,7 +122,7 @@ function endRound() {
       currentMonsterHealth
     );
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("Draw! Both Lost");
+    declareWinner("DRAW! BOTH LOST");
     writeToLog(
       LOG_GAME_OVER,
       "DRAW!",
@@ -121,7 +132,7 @@ function endRound() {
   }
 
   if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
-    reset();
+    disableBtns(true);
   }
 }
 
@@ -186,7 +197,181 @@ function printLogHandler() {
   }
 }
 
+// function with arguments and default value
+const greet = (greetMessage = "Namaste!", userName) => {
+  return `${greetMessage} ${userName}`;
+};
+console.log(greet(undefined, "Krishna"));
+
+// function without arguments hardcoded message
+const greetAgain = () => {
+  console.log("Hi! Welcome Back");
+};
+greetAgain();
+
+// if all the strings is not empty throw an alert
+const checkInput = (cb, ...inputVal) => {
+  console.log(inputVal);
+  let isEmpty = false;
+  for (inptVal of inputVal) {
+    if (!inptVal) {
+      //checks if string is empty or not
+      isEmpty = true;
+      break;
+    }
+  }
+  if (isEmpty) {
+    cb();
+  }
+};
+checkInput(
+  () => console.log("String is not empty!"),
+  "J",
+  "a",
+  "v",
+  "a",
+  "s",
+  "c",
+  "r",
+  "i",
+  "p",
+  "t"
+);
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHandler);
 logBtn.addEventListener("click", printLogHandler);
+restartGame.addEventListener("click", restartGameHandler);
+
+const getPlayerChoice = () => {
+  // prompt option to allow user to enter his/hers option
+  const selectedOption = prompt(
+    `Choose ${ROCK}, ${PAPER} OR ${SCISSOR}`,
+    ""
+  ).toUpperCase();
+  // if selected option is not rock or paper or scissor show below alert and return nothing
+  if (
+    selectedOption !== ROCK &&
+    selectedOption !== PAPER &&
+    selectedOption !== SCISSOR
+  ) {
+    alert(`Invalid option. Default option set is ${ROCK}`);
+    return;
+  }
+  return selectedOption;
+};
+
+// computer choice gets dynamically generated using Math.random()
+const getComputerChoice = () => {
+  const choice = Math.random();
+  if (choice < 0.3) {
+    return ROCK;
+  } else if (choice < 0.6) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+};
+
+// gets the winner based on player and computer choice
+// if player choice is not selected or invalid ROCK option is selected by default
+const getWinner = (compChoice, plyrChoice = ROCK) =>
+  plyrChoice === compChoice
+    ? DRAW
+    : (plyrChoice === PAPER && compChoice === ROCK) ||
+      (plyrChoice === SCISSOR && compChoice === PAPER) ||
+      (plyrChoice === ROCK && compChoice === SCISSOR)
+    ? PLAYER_WON
+    : COMPUTER_WON;
+
+startGame2.addEventListener("click", () => {
+  console.log("Game 2 Started!");
+  const playerOption = getPlayerChoice();
+  const computerOption = getComputerChoice();
+  let winner;
+  // saves the winner
+  if (playerOption) {
+    winner = getWinner(computerOption, playerOption);
+  } else {
+    winner = getWinner(computerOption);
+  }
+
+  // displays the message along with options selected by player and computer
+  const message = `Player Selected ${
+    playerOption || ROCK
+  } Computer Selected ${computerOption}. `;
+
+  if (winner === PLAYER_WON) {
+    alert(`${message} PLAYER WON!`);
+  } else if (winner === COMPUTER_WON) {
+    alert(`${message} COMPUTER WON!`);
+  } else {
+    alert(`${message} DRAW!`);
+  }
+});
+
+// displaying name based on dynamic characters
+const getName = (...characters) => {
+  //Rest operator will convert the dynamic arguments to array
+  console.log(characters);
+  let alphabet = "";
+  for (character of characters) {
+    alphabet += character;
+  }
+  return alphabet;
+};
+console.log("Total = " + getName("K", "R", "I", "S", "H", "N", "A"));
+
+// example of call back function
+const calc = (finalResult, operation, ...numbers) => {
+  const checkEmptyInput = (number) => (isNaN(number) ? 0 : number);
+  let reslt = 0;
+  // let msg = "";
+  if (operation === "ADD") {
+    // msg = "Result after addition";
+    for (number of numbers) {
+      reslt += checkEmptyInput(number);
+    }
+  } else if (operation === "SUBTRACT") {
+    // msg = "Result after subtraction";
+    for (number of numbers) {
+      reslt -= checkEmptyInput(number);
+    }
+  }
+  finalResult(reslt);
+};
+
+// function which will be called as callback function
+const outputResult = (message, result) => {
+  alert(`${message} ${result}`);
+};
+
+calc(
+  outputResult.bind(this, "Result after addition"),
+  "ADD",
+  10,
+  20,
+  30,
+  40,
+  "Hello",
+  "Hi"
+);
+calc(
+  outputResult.bind(this, "Result after addition"),
+  "ADD",
+  10,
+  20,
+  30,
+  40,
+  100,
+  50
+);
+calc(
+  outputResult.bind(this, "Result after subtraction"),
+  "SUBTRACT",
+  100,
+  50,
+  10,
+  5
+);
--- JS - Self Learning/assets/scripts/vendor.js
+++ JS - Self Learning/assets/scripts/vendor.js
@@ -1,11 +1,13 @@
-const monsterHealthBar = document.getElementById('monster-health');
-const playerHealthBar = document.getElementById('player-health');
-const bonusLifeEl = document.getElementById('bonus-life');
-
-const attackBtn = document.getElementById('attack-btn');
-const strongAttackBtn = document.getElementById('strong-attack-btn');
-const healBtn = document.getElementById('heal-btn');
-const logBtn = document.getElementById('log-btn');
+const monsterHealthBar = document.getElementById("monster-health");
+const playerHealthBar = document.getElementById("player-health");
+const bonusLifeEl = document.getElementById("bonus-life");
+const attackBtn = document.getElementById("attack-btn");
+const strongAttackBtn = document.getElementById("strong-attack-btn");
+const healBtn = document.getElementById("heal-btn");
+const logBtn = document.getElementById("log-btn");
+const matchStatus = document.getElementById("winner");
+const restartGame = document.getElementById("restart-btn");
+const startGame2 = document.getElementById("start-game");
 
 function adjustHealthBars(maxLife) {
   monsterHealthBar.max = maxLife;
@@ -33,6 +35,8 @@ function increasePlayerHealth(healValue) {
 function resetGame(value) {
   playerHealthBar.value = value;
   monsterHealthBar.value = value;
+  disableBtns(false);
+  declareWinner("");
 }
 
 function removeBonusLife() {
@@ -41,4 +45,20 @@ function removeBonusLife() {
 
 function setPlayerHealth(health) {
   playerHealthBar.value = health;
+}
+
+function declareWinner(winner) {
+  matchStatus.textContent = winner;
+}
+
+function disableBtns(isDiabled) {
+  if (isDiabled) {
+    attackBtn.disabled = true;
+    strongAttackBtn.disabled = true;
+    healBtn.disabled = true;
+  } else {
+    attackBtn.disabled = false;
+    strongAttackBtn.disabled = false;
+    healBtn.disabled = false;
+  }
 }
